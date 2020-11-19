4b1f7e4421be650accbf58c4d0eb146a71473beb
Alberto Ferrari <alberto.ferrari@unipr.it>
2020-11-19 16:58:32 +0100

    Add files via upload

---
+++ esercizi/database/Arte_Query_Simulazione CISITA 2020.sql
@@ -0,0 +1,30 @@
+USE arte;
+
+-- 1. Alias e anno di nascita di tutti gli artisti
+
+
+-- 2. Nome dei musei di Parigi
+
+
+-- 3. Titolo dei quadri dipinti con tecnica 
+--    "Olio su tela" prima del 1700
+
+
+-- 4. Elenco degli artisti ordinati alfabeticamente per Alias
+
+
+-- 5. Modificare in 1502 l'anno di esecuzione del quadro "La Tempesta" di Giorgione
+
+
+-- 6. Alias dell'artista e titolo dei quadri del XVII secolo
+
+
+-- 7. Aggiornare il database includendo i dati desunti dalla seguente informazione:
+--    "la prima versione della Vergine delle Rocce è un dipinto a olio su tavola 
+--    (199x122 cm) di Leonardo da Vinci, databile al 1483 e conservato 
+--    nel Musée du Louvre di Parigi" 
+
+-- 8. Nome e Alias dell'autore, titolo e anno si esecuzione del quadro e nome del museo
+--    di tutte le opere presenti nel database
+
+
